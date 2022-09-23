110ffa28467ea4816d1c86e589d66ddb753e99eb
Yaser Andrade <77845161+Yaser-AndOr@users.noreply.github.com>
2022-09-22 19:58:07 -0600

    Ejercicio 2

--- FioDioMySQL.sql
+++
@@ -1,5 +0,0 @@
-use costarica;
-select *, SUBSTRING(codigo_cliente, 1, 2) as CódigoPaís from cliente where idtipocliente = 1 or idtipocliente = 2;
-
-use costarica;
-select *, SUBSTRING(codigo_cliente, 1, 2) as CódigoPaís from cliente where idtipocliente = 3 or idtipocliente = 4;
--- FioDioSQLServer.sql
+++
@@ -1,5 +0,0 @@
-use db_roaming_honduras
-select *, SUBSTRING(codigo_cliente, 1, 2) as [Código País] from cliente where idtipocliente = 1 or idtipocliente = 2
-
-use db_roaming_honduras
-select *, SUBSTRING(codigo_cliente, 1, 2) as [Código País] from cliente where idtipocliente = 4 or idtipocliente = 3
